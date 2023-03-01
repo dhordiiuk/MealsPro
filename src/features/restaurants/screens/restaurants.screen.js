b59e7782751790dd0bccefac68ca68c4b7900fe0
dariiahordiiuk <dgordiuk@gmail.com>
2023-03-01 15:10:16 -0800

    add ability to render different restaurants and search with rerendering them out

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,55 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, SafeAreaView, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
+import { ActivityIndicator } from "react-native-paper";
 
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Search } from "../components/search.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import {colors} from "../../../infrastructure/theme/colors";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+
+const RestaurantList = styled(FlatList).attrs({
+    contentContainerStyle: {
+        padding: 16,
+    },
+})``;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `;
 
-const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-export const RestaurantsScreen = () => (
-    <SafeArea>
-        <SearchContainer>
-            <Searchbar />
-        </SearchContainer>
-        <FlatList
-            data={[
-                { name: 1 },
-                { name: 2 },
-                { name: 3 },
-                { name: 4 },
-                { name: 5 },
-                { name: 6 },
-                { name: 7 },
-                { name: 8 },
-                { name: 9 },
-                { name: 10 },
-                { name: 11 },
-                { name: 12 },
-                { name: 13 },
-                { name: 14 },
-            ]}
-            renderItem={(k) => (
-                <Spacer position="bottom" size="large">
-                    <RestaurantInfoCard key={k.name}/>
-                </Spacer>
+export const RestaurantsScreen = () => {
+    const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+    return (
+        <SafeArea>
+            {isLoading && (
+                <LoadingContainer>
+                    <Loading size={50} animating={true} />
+                </LoadingContainer>
             )}
-            keyExtractor={(item) => item.name}
-            contentContainerStyle={{ padding: 16 }}
-        />
-    </SafeArea>
-);
+            <Search />
+            <RestaurantList
+                data={restaurants}
+                renderItem={({ item }) => {
+                    return (
+                        <Spacer position="bottom" size="large">
+                            <RestaurantInfoCard restaurant={item} />
+                        </Spacer>
+                    );
+                }}
+                keyExtractor={(item) => item.name}
+            />
+        </SafeArea>
+    );
+};
