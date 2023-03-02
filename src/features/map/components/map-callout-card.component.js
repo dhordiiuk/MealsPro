0f18e31c76d2f12f304d1eaba936808afcc2d912
dariiahordiiuk <dgordiuk@gmail.com>
2023-03-01 17:18:56 -0800

    add map, search, markers, etc.

---
+++ src/features/map/components/map-callout-card.component.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
+
+export const MapCalloutCard = ({ restaurant }) => (
+    <CompactRestaurantInfo restaurant={restaurant} />
+);
