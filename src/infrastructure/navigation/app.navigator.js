cf72ee9ca516ce0f128724c3fac2d0594b77e3c9
dariiahordiiuk <dgordiuk@gmail.com>
2023-03-02 17:51:36 -0800

    add logout & session expired button

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
 
 import { SafeArea } from "../../components/utility/safe-area.component";
 
+import { AuthenticationContext } from "../../services/authentification/authentication.context";
 import { RestaurantsNavigator } from "./restaurants.navigator";
 import { MapScreen } from "../../features/map/screens/map.screen";
 
@@ -16,11 +17,15 @@ const TAB_ICON = {
     Settings: "md-settings",
 };
 
-const Settings = () => (
-    <SafeArea>
-        <Text>Settings</Text>
-    </SafeArea>
-);
+const Settings = () => {
+    const {onLogout} = useContext(AuthenticationContext);
+    return (
+        <SafeArea>
+            <Text>Settings</Text>
+            <Button title="logout" onPress={() => onLogout()}/>
+        </SafeArea>
+    );
+};
 
 const createScreenOptions = ({ route }) => {
     const iconName = TAB_ICON[route.name];
